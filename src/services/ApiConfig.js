9ff590b1baa1de4db011c438c6de5f7a1e3a8ae7
neeraj-veeranki <36732562+neeraj-veeranki@users.noreply.github.com>
2021-04-15 12:43:20 +0200

    Update ApiConfig.js

--- src/services/ApiConfig.js
+++ src/services/ApiConfig.js
@@ -1,7 +1,7 @@
 // Service config for the application.
 const ApiConfig = {
     'apiBaseUrl':'https://api.exchangeratesapi.io/',
-    'apiKey': '577019210e2f15405c0ad3918f8133c6',
+    'apiKey': '12344fgr44',
     'targetCurrencies': 'USD,EUR,BRL,GBP,AUD'
 }
 
